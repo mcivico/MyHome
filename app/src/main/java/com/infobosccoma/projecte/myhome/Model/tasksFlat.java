437d2316feaad93262e6523a3a4fc651bbad0666
mcivico <mcivico@infobosccoma.net>
2015-05-08 18:30:34 +0200

    Emplenar dades tasks

--- app/src/main/java/com/infobosccoma/projecte/myhome/Model/tasksFlat.java
+++ app/src/main/java/com/infobosccoma/projecte/myhome/Model/tasksFlat.java
@@ -5,5 +5,36 @@ package com.infobosccoma.projecte.myhome.Model;
  */
 public class tasksFlat {
 
+    private int idTasks, idFlatTask;
+    private String nameTask;
 
+    public tasksFlat(int idTasks, int idFlatTask, String nameTask) {
+        this.idTasks = idTasks;
+        this.idFlatTask = idFlatTask;
+        this.nameTask = nameTask;
+    }
+
+    public int getIdTasks() {
+        return idTasks;
+    }
+
+    public void setIdTasks(int idTasks) {
+        this.idTasks = idTasks;
+    }
+
+    public int getIdFlatTask() {
+        return idFlatTask;
+    }
+
+    public void setIdFlatTask(int idFlatTask) {
+        this.idFlatTask = idFlatTask;
+    }
+
+    public String getNameTask() {
+        return nameTask;
+    }
+
+    public void setNameTask(String nameTask) {
+        this.nameTask = nameTask;
+    }
 }
