fa886172eca393800fdad1c23207363771569625
nan <nancansagal@msn.com>
2015-05-15 15:07:22 +0200

    hola

--- app/src/main/java/com/infobosccoma/projecte/myhome/Login.java
+++ app/src/main/java/com/infobosccoma/projecte/myhome/Login.java
@@ -1,13 +1,11 @@
 package com.infobosccoma.projecte.myhome;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Vibrator;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -34,7 +32,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class Login extends ActionBarActivity implements View.OnClickListener {
+public class Login extends Activity implements View.OnClickListener {
 
     private static final String URL_DATA = "http://52.16.108.57/scripts/users.php";
 
@@ -68,27 +66,8 @@ public class Login extends ActionBarActivity implements View.OnClickListener {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     public void onClick(View v) {
